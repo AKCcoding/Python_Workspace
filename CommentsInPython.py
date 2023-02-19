0eb5440c2646d445ab4525d51f980b91747f998d
AKCcoding <alraeatwork@gmail.com>
2023-02-19 23:16:22 +0800

    Added Exercises in Python

---
+++ CommentsInPython.py
@@ -0,0 +1,3 @@
+#Don't be verbose and Repititive
+#Dont Explain how the code works if its obvious
+#Everytime the code change so does the comment update and modify it.
--- Exceptions.py
+++ Exceptions.py
@@ -1,2 +1,9 @@
-age = int(input('Age: '))
-print(age)
+try:
+    age = int(input('Age: '))
+    print(age)
+    salary = 20000
+    risk = salary/age
+except ValueError:
+    print('Invalid Value')
+except ZeroDivisionError:
+    print('Age cannot be 0')
