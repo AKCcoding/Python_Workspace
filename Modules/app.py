4aeee9274c5441b659cae8a7867dd17bb98c6942
AKCcoding <alraeatwork@gmail.com>
2023-06-30 10:59:02 +0800

    remove some broken files

--- Modules/app.py
+++
@@ -1,8 +0,0 @@
-# One way to use a module is to import it
-import converter
-# This is the other way to import a module but selected functions
-from converter import kg_to_lbs
-from converter import lbs_to_kg
-
-print(converter.kg_to_lbs(70))
-print(converter.lbs_to_kg(154))
--- Modules/converter.py
+++
@@ -1,5 +0,0 @@
-def lbs_to_kg(weight):
-    return weight * 0.45
-
-def kg_to_lbs(weight):
-    return weight / 0.45
